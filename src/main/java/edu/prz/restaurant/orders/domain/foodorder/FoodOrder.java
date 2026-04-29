6ea38cbe409e07cedad4e62e11613e8e9ed42946
Maciej Koryl <maciej.koryl@asseco.pl>
2026-04-29 16:28:25 +0200

    Kontekst orders - początki

--- src/main/java/edu/prz/restaurant/orders/domain/foodorder/FoodOrder.java
+++ src/main/java/edu/prz/restaurant/orders/domain/foodorder/FoodOrder.java
@@ -2,8 +2,10 @@ package edu.prz.restaurant.orders.domain.foodorder;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import lombok.Data;
 
 @Entity
+@Data
 public class FoodOrder {
 
   @Id
